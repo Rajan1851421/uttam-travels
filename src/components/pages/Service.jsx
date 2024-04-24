50c074110851667a03fd428d6814b562ad1f12fb
Rajan Prajapati <rajanprajapati743@gmail.com>
2024-04-24 17:18:08 +0530

    add services

--- src/components/pages/Service.jsx
+++ src/components/pages/Service.jsx
@@ -1,11 +1,14 @@
 
 function Service() {
   return (
-    <div className='h-screen'>
-        <div className='mt-14'>
-            <h2>Sevice Page</h2>
-
-        </div>
+    <div className=''>
+      <div className=" ">
+        <img className="h-[500px] w-full" src="https://chikucab.com/images/home/slidebg.webp" alt="" />
+      </div>
+      <div className="bg-gradient-to-r from-rose-100 to-teal-100  ">
+        <h3 className="mx-auto text-center text-xl md:text-4xl font-bold capitalize ">we are Provide Best Services</h3>
+        
+      </div>
       
     </div>
   )
