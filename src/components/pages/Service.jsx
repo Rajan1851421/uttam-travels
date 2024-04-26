fb845528e718988d2b1068a3d77eedc25771d485
Rajan Prajapati <rajanprajapati743@gmail.com>
2024-04-26 17:54:57 +0530

    service page add

--- src/components/pages/Service.jsx
+++ src/components/pages/Service.jsx
@@ -5,11 +5,60 @@ function Service() {
       <div className=" ">
         <img className="h-[500px] w-full" src="https://chikucab.com/images/home/slidebg.webp" alt="" />
       </div>
-      <div className="bg-gradient-to-r from-rose-100 to-teal-100  ">
-        <h3 className="mx-auto text-center text-xl md:text-4xl font-bold capitalize ">we are Provide Best Services</h3>
-        
+      <div className=" ">
+        <div className="p-8 roumded-lg shadow-lg bg-[#FEF9C3] m-10  ">
+          <div className="mx-auto ">
+            <h1 className="font-bold text-lg md:text-3xl uppercase text-[#78716C] ">Jackpot Deals on Top Selling Packages</h1>
+            <p className="text-[#A8A29E] text-md ">Save extra with our exclusive deals!</p>
+            <div className="grid grid-cols-2 md:grid-cols-7 mt-4  gap-2">
+              <div className="relative">
+                <img className="rounded-md shadow-md block w-full" loading="lazy" src="https://hldak.mmtcdn.com/prod-s3-hld-hpcmsadmin/holidays/images/cities/3052/Beautiful%20tropical%20beach%20in%20Thailand.jpg?crop=134:207&downsize=134:207" alt="" />
+                <div className="absolute inset-x-0 bottom-0 h-8 bg-gradient-to-t
+                 from-gray-950 to-transparent mx-auto text-center text-white font-semibold">Thiland 5N 1000/-</div>
+              </div>
+
+              <div className="relative">
+                <img className="rounded-md shadow-md block w-full" loading="lazy" src="https://hldak.mmtcdn.com/prod-s3-hld-hpcmsadmin/holidays/images/cities/1328/ZF.jpg?crop=134:207&downsize=134:207" alt="" />
+                <div className="absolute inset-x-0 bottom-0 h-8
+                 bg-gradient-to-t from-gray-800 to-transparent mx-auto text-center text-white font-semibold">Goa 4N 15000/- </div>
+              </div>
+              <div className="relative">
+                <img className="rounded-md shadow-md block w-full" loading="lazy" src="https://hldak.mmtcdn.com/prod-s3-hld-hpcmsadmin/holidays/images/cities/1369/Verdant%20tea%20plantations%20of%20Munnar.jpg?crop=134:207&downsize=134:207" alt="" />
+                <div className="absolute inset-x-0 bottom-0 h-8 bg-gradient-to-t from-gray-800 to-transparent mx-auto text-center text-white font-semibold">
+                  Kerala 4N 20000/-
+                </div>
+              </div>
+              <div className="relative">
+                <img className="rounded-md shadow-md block w-full" loading="lazy" src="https://hldak.mmtcdn.com/prod-s3-hld-hpcmsadmin/holidays/images/cities/1404/Pura-Ulun-Danu-temple.jpg?crop=134:207&downsize=134:207" alt="" />
+                <div className="absolute inset-x-0 bottom-0 h-8 bg-gradient-to-t from-gray-800 to-transparent mx-auto text-center text-white font-semibold">
+                  Bali 4N 124578/-
+                </div>
+              </div>
+              <div className="relative">
+                <img className="rounded-md shadow-md block w-full" loading="lazy" src="https://hldak.mmtcdn.com/prod-s3-hld-hpcmsadmin/holidays/images/cities/3147/DTCM_The%20Address%20JBR_Night.jpg?crop=134:207&downsize=134:207" alt="" />
+                <div className="absolute inset-x-0 bottom-0 h-8 bg-gradient-to-t from-gray-800 to-transparent mx-auto text-center text-white font-semibold">
+                  Dubai 5N 500000/-
+                </div>
+              </div>
+              <div className="relative">
+                <img className="rounded-md shadow-md block w-full" loading="lazy" src="https://hldak.mmtcdn.com/prod-s3-hld-hpcmsadmin/holidays/images/cities/4308/Houseboat%20on%20Dal%20Lake.jpg?crop=134:207&downsize=134:207" alt="" />
+                <div className="absolute inset-x-0 bottom-0 h-8 bg-gradient-to-t from-gray-800 to-transparent mx-auto text-center text-white font-semibold">
+                  Kashmir 6N 45000/-
+                </div>
+              </div>
+              <div className="relative">
+                <img className="rounded-md shadow-md block w-full" loading="lazy" src="https://hldak.mmtcdn.com/prod-s3-hld-hpcmsadmin/holidays/images/cities/3736/Yen-stream-in-Autumn.jpg?crop=134:207&downsize=134:207" alt="" />
+                <div className="absolute inset-x-0 bottom-0 h-8 bg-gradient-to-t from-gray-800 to-transparent mx-auto text-center text-white font-semibold">
+                  Vietanam 6N 65000/-
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
       </div>
-      
+
     </div>
   )
 }
