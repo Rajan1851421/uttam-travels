54d6a1dfc321210519793d99c8c095f05c271d97
Rajan Prajapati <rajanprajapati743@gmail.com>
2024-05-04 19:11:54 +0530

    again home logo routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,75 +33,77 @@ function Navbar() {
     <nav className="bg-gray-800 py-2 sticky top-0 left-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <img className='h-14 w-14 rounded-full' src="https://dynamic.brandcrowd.com/asset/logo/51511b41-8f8a-4077-b445-137cca9f9c46/logo-search-grid-1x?logoTemplateVersion=1&v=638446128566130000&text=Uttam+Travels" alt="" />
+          <Link to='/' className='flex justify-start'>  <img className='h-14 w-14 rounded-full' src="https://dynamic.brandcrowd.com/asset/logo/51511b41-8f8a-4077-b445-137cca9f9c46/logo-search-grid-1x?logoTemplateVersion=1&v=638446128566130000&text=Uttam+Travels" alt="" />
+        </Link>
 
+        {/* Mobile menu button */}
+        <div className="flex md:hidden">
+          <button onClick={toggleMenu} className="text-white p-2 focus:outline-none">
+            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+        </div>
 
-          {/* Mobile menu button */}
-          <div className="flex md:hidden">
-            <button onClick={toggleMenu} className="text-white p-2 focus:outline-none">
-              <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-              </svg>
-            </button>
-          </div>
-
-          {/* Desktop menu items */}
-          <div className="hidden md:block">
-            <div className="flex space-x-4">
-              <Link to="/"  className="text-white hover:bg-gray-700 px-3 py-2 rounded-md flex justify-center items-center "><RiHome4Fill className='mx-1 text-[#84CC16]' />Home</Link>
-              <Link to="/service"  className="text-white hover:bg-gray-700 px-3 py-2 rounded-md flex justify-center items-center"> <FcServices className='mx-1' /> Services</Link>
-              <Link to="/contact"  className="text-white hover:bg-gray-700 px-3 py-2 rounded-md flex justify-center items-center "><IoIosCall className='mx-1' /> Contact</Link>
-              <Link to="/about"  className="text-white hover:bg-gray-700 px-3 py-2 rounded-md flex justify-center items-center "><FcAbout className='mx-1' /> About Us</Link>
-             {
+        {/* Desktop menu items */}
+        <div className="hidden md:block">
+          <div className="flex space-x-4">
+            <Link to="/" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md flex justify-center items-center "><RiHome4Fill className='mx-1 text-[#84CC16]' />Home</Link>
+            <Link to="/service" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md flex justify-center items-center"> <FcServices className='mx-1' /> Services</Link>
+            <Link to="/contact" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md flex justify-center items-center "><IoIosCall className='mx-1' /> Contact</Link>
+            <Link to="/about" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md flex justify-center items-center "><FcAbout className='mx-1' /> About Us</Link>
+            {
               Token_login ?
-              (
-                <Link to="/vechile"  className="text-white hover:bg-gray-700 px-3 py-2 rounded-md flex justify-center items-center "><FaCar className='mx-1' /> Add Vehicle</Link>
+                (
+                  <Link to="/vechile" className="text-white hover:bg-gray-700 px-3 py-2 rounded-md flex justify-center items-center "><FaCar className='mx-1' /> Add Vehicle</Link>
 
-              ):
-              null
-             }
-             
-              {
-                Token_login ?
-                  (<button onClick={handleLogout} className='bg-[#B91C1C] px-2 rounded-md text-white flex justify-center items-center '> <TbLogout className='mx-1 text-[#DCFCE7]' /> Logout</button>) :
-                  <Link to="/login"  className="text-white hover:bg-gray-700 px-3 py-2
+                ) :
+                null
+            }
+
+            {
+              Token_login ?
+                (<button onClick={handleLogout} className='bg-[#B91C1C] px-2 rounded-md text-white flex justify-center items-center '> <TbLogout className='mx-1 text-[#DCFCE7]' /> Logout</button>) :
+                <Link to="/login" className="text-white hover:bg-gray-700 px-3 py-2
                   border rounded-md flex justify-center items-center "><FaRegUserCircle className='mx-1' /> Login</Link>
-              }
+            }
 
 
-            </div>
           </div>
         </div>
       </div>
+    </div>
 
-      {/* Mobile menu */}
-      {isOpen && (
-        <div className="md:hidden">
-          <div className="px-2 pt-2 pb-3 space-y-1">
-            <Link to="/" onClick={closeMenu} className="text-white  px-3 py-2 rounded-md flex justify-center items-center "><RiHome4Fill className='mx-1 text-[#84CC16]' />Home</Link>
-            <Link to="/service" onClick={closeMenu} className="text-white  px-3 py-2 rounded-md flex justify-center items-center "><FcServices className='mx-1' /> Services</Link>
-            <Link to="/contact" onClick={closeMenu} className="text-white  px-3 py-2 rounded-md flex justify-center items-center "><IoIosCall className='mx-1' /> Contact</Link>
-            <Link to="/about" onClick={closeMenu} className="text-white  px-3 py-2 rounded-md flex justify-center items-center "><FcAbout className='mx-1' /> About Us</Link>
-           
-            {
-              Token_login ?
+      {/* Mobile menu */ }
+  {
+    isOpen && (
+      <div className="md:hidden">
+        <div className="px-2 pt-2 pb-3 space-y-1">
+          <Link to="/" onClick={closeMenu} className="text-white  px-3 py-2 rounded-md flex justify-center items-center "><RiHome4Fill className='mx-1 text-[#84CC16]' />Home</Link>
+          <Link to="/service" onClick={closeMenu} className="text-white  px-3 py-2 rounded-md flex justify-center items-center "><FcServices className='mx-1' /> Services</Link>
+          <Link to="/contact" onClick={closeMenu} className="text-white  px-3 py-2 rounded-md flex justify-center items-center "><IoIosCall className='mx-1' /> Contact</Link>
+          <Link to="/about" onClick={closeMenu} className="text-white  px-3 py-2 rounded-md flex justify-center items-center "><FcAbout className='mx-1' /> About Us</Link>
+
+          {
+            Token_login ?
               (
-                <Link to="/vechile" onClick={closeMenu}   className="text-white hover:bg-gray-700 px-3 py-2 rounded-md flex justify-center items-center "><FaCar className='mx-1' /> Add Vehicle</Link>
+                <Link to="/vechile" onClick={closeMenu} className="text-white hover:bg-gray-700 px-3 py-2 rounded-md flex justify-center items-center "><FaCar className='mx-1' /> Add Vehicle</Link>
 
-              ):
+              ) :
               null
-             } 
-            {
-                Token_login ?
-                  (<button onClick={handleLogout} className='bg-[#B91C1C] px-2 rounded-md text-white flex justify-center items-center '> <TbLogout className='mx-1 text-[#DCFCE7]' /> Logout</button>) 
-                  :
-                 ( <Link to="/login" onClick={closeMenu}   className="text-white hover:bg-gray-700 px-3 py-2
+          }
+          {
+            Token_login ?
+              (<button onClick={handleLogout} className='bg-[#B91C1C] px-2 rounded-md text-white flex justify-center items-center '> <TbLogout className='mx-1 text-[#DCFCE7]' /> Logout</button>)
+              :
+              (<Link to="/login" onClick={closeMenu} className="text-white hover:bg-gray-700 px-3 py-2
                   border rounded-md flex justify-center items-center "><FaRegUserCircle className='mx-1' /> Login</Link>)
-              }
-          </div>
+          }
         </div>
-      )}
-    </nav>
+      </div>
+    )
+  }
+    </nav >
   );
 }
 
