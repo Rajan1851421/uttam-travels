dda3fe848689583ab5ff140b9d1301d69d10c59f
Rajan Prajapati <rajanprajapati743@gmail.com>
2024-07-17 17:38:21 +0530

    view vehicle agin update

--- src/components/pages/ViewVehicle.jsx
+++ src/components/pages/ViewVehicle.jsx
@@ -4,13 +4,13 @@ import { useSelector } from "react-redux";
 
 const Modal = () => {
     const [showModal, setShowModal] = useState(true);
-    const { ViewVechile,loading } = useSelector((state) => state.productStore);
-   
+    const { ViewVechile, loading } = useSelector((state) => state.productStore);
+
     const navigate = useNavigate()
 
     useEffect(() => {
-       
-       
+
+
     }, [])
     const handleCloseModal = () => {
         setShowModal(false)
@@ -18,28 +18,31 @@ const Modal = () => {
 
     }
 
-    if(loading){
+    if (loading) {
         <h1>Loading....</h1>
     }
 
     return (
         <>
-
-
             {showModal && (
                 <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
                     <div className="relative w-auto max-w-3xl mx-auto my-6">
-                       
+                        {/* ... */}
                         <div className="relative flex flex-col w-full p-2 bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
-                          <h1>Scorpio</h1>
-
-                            <p className="text-center text-md sm:text-sm lg-text-md font-bold underline uppercase ">{ViewVechile && ViewVechile.Vehicle.carName}</p>
+                            
+                            {ViewVechile && ViewVechile.Vehicle && (
+                                <p className="text-center text-md sm:text-sm lg-text-md font-bold underline uppercase ">
+                                    {ViewVechile.Vehicle.carName}
+                                </p>
+                            )}
                             <div>
-                                <img src={ViewVechile && ViewVechile.Vehicle.image} alt="image" />
+                                <img src={ViewVechile && ViewVechile.Vehicle && ViewVechile.Vehicle.image} alt="image" />
                             </div>
-                            <p className="text-center text-md sm:text-sm lg-text-md font-bold   ">{`Rate : ${ViewVechile && ViewVechile.Vehicle.rate} Km`}</p>
-
-                           
+                            {ViewVechile && ViewVechile.Vehicle && (
+                                <p className="text-center text-md sm:text-sm lg-text-md font-bold ">
+                                    {`Rate : ${ViewVechile.Vehicle.rate} Km`}
+                                </p>
+                            )}
                             <div className="flex items-center justify-end p-6 border-t border-solid border-gray-300 rounded-b">
                                 <button
                                     onClick={handleCloseModal}
@@ -52,6 +55,9 @@ const Modal = () => {
                     </div>
                 </div>
             )}
+
+
+          
 
             {showModal && (
                 <div
